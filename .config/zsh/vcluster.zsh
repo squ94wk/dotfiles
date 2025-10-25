87c83d0482619593fcd089525fc6bccf0d84e7b4
squ94wk <squ94wk@gmail.com>
2025-10-25 17:44:37 +0200

    Update vcluster shell integration

--- .config/zsh/vcluster.zsh
+++ .config/zsh/vcluster.zsh
@@ -6,3 +6,38 @@ if [[ "$UPDATE" == "true" ]]; then
 fi
 
 compdef _vcluster v
+
+build_load_pro () {
+	local version=$1
+	git co "v${version}"
+	just build-snapshot
+	docker tag ghcr.io/loft-sh/vcluster-pro:dev-next ghcr.io/loft-sh/vcluster-pro:${version}
+	kind load docker-image --name airgapped ghcr.io/loft-sh/vcluster-pro:${version}
+}
+
+vendor_vcluster () {
+	echo "==> Updating vcluster repository..."
+	cd ~loft/vcluster
+
+	if [[ "$(git this)" == "main" ]]; then
+		echo "On main branch, running git ffff..."
+		git ffff
+	else
+		echo "Not on main branch, running git fm..."
+		git fm
+	fi
+
+	local commit_hash=${1:-$(git rev-parse main)}
+	echo "==> Using commit: $commit_hash"
+
+	echo "==> Updating vcluster-pro dependencies..."
+	cd ~loft/vcluster-pro
+
+	echo "==> Running go get..."
+	go get github.com/loft-sh/vcluster@"$commit_hash"
+
+	echo "==> Running go mod vendor..."
+	go mod vendor
+
+	echo "==> Done! vcluster-pro now vendors vcluster@$commit_hash"
+}
